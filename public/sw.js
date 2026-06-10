0c6a8cc2f53043d765a5be500307a2e84c88679f
Khizr Amir <75015077+khizramir@users.noreply.github.com>
2026-06-10 22:05:16 +0500

    Add service worker

---
+++ public/sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'halal-hub-v1';
+const STATIC_ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener('push', (event) => {
+  const data = event.data?.json() ?? {};
+  event.waitUntil(
+    self.registration.showNotification(data.title || 'Prayer Time', {
+      body: data.body || 'Time to pray',
+      icon: '/icon-192.png',
+      badge: '/icon-192.png',
+    })
+  );
+});
